dd16b2555793026e36ae3e506652506a7c9a24d2
KwFung7 <lingfung09@hotmail.com>
2018-02-15 20:59:29 +0800

    [ServerLog] set middleware

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const fs = require('fs')
 const { Portfolio } = require('./model')
 // const portfolioExample = require('./example/portfolioExample');
+
+
+/** ============================================================= *
+  * Middleware
+  * ============================================================= */
 let app = express();
+app.use((req, res, next) => {
+  // for server log
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', `${log}\n`, err => {
+    console.log('Unable to append server.log');
+  });
+  next();
+});
+// app.use('/', express.static(`${__dirname}/client/build`));
+
 
 /** ============================================================= *
   * Request
   * ============================================================= */
-// app.use('/', express.static(`${__dirname}/client/build`));
 app.get('/api/v1/hello', (req, res) => {
   res.send({ testingText: 'Testing api !'});
 });
