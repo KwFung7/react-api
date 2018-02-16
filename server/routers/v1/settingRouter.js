cb44879818914b702a55b4f3f0f75d33822b7e52
KwFung7 <lingfung09@hotmail.com>
2018-02-16 19:42:51 +0800

    [CORS] support cross origin resource sharing for setting

--- server/routers/v1/settingRouter.js
+++ server/routers/v1/settingRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const HttpStatus = require('http-status-codes');
 const { Setting } = require('../../models');
 let router = express.Router();
 
 const { OK, BAD_REQUEST } = HttpStatus
-router.get('/setting', (req, res) => {
+router.get('/setting', cors(), (req, res) => {
   Setting.find()
     .then((settings) => {
       res.status(OK).send(settings);
