51ac3fdc9e393e0532918287f63e68f225fb93a4
KwFung7 <lingfung09@hotmail.com>
2018-02-18 00:12:08 +0800

    [Build] build client / express use static build

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   });
   next();
 });
-// app.use('/', express.static(`${__dirname}/client/build`));
+app.use('/', express.static(`${__dirname}/client/build`));
 app.use('/', ApiRouter);
 
 
