194b81731ca5a4f5f2c1931278292b6de7b14f68
Felix Kwan <kwfxng@gmail.com>
2020-09-19 16:21:53 +0800

    [AWS] commit FE build as well

--- server/db-connect.js
+++ server/db-connect.js
@@ -1,8 +1,14 @@
 let mongoose = require('mongoose');
+const fs = require('fs');
 const dbConnection = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConnection).then(() => {
+mongoose.connect(dbConnection, {
+  useNewUrlParser: true,
+  ssl: true,
+  sslValidate: false,
+  sslCA: fs.readFileSync('./rds-combined-ca-bundle.pem')
+}).then(() => {
   console.log('Connected to MongoDB.');
 }, (err) => {
   console.log(err);
